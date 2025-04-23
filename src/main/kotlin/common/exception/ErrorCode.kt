d6e52fef52c45c7a0e8d4796ac09af27ccb790a9
bak3839 <bak3839@naver.com>
2025-04-24 02:14:42 +0900

    JWT 생성 및 검증 구현

--- src/main/kotlin/common/exception/ErrorCode.kt
+++ src/main/kotlin/common/exception/ErrorCode.kt
@@ -12,5 +12,8 @@ enum class ErrorCode(
 ): CodeInterface {
     AUTH_CONFIG_NOT_FOUND(-100, "Auth config not found"),
     FAILED_TO_CALL_CLIENT(-101, "Failed to call client"),
-    CALL_RESULT_BODY_NULL(-102, "Body is nil")
+    CALL_RESULT_BODY_NULL(-102, "Body is nil"),
+    PROVIDER_NOT_FOUND(-103, "Provider not found"),
+    TOKEN_IS_INVALID(-104, "Token invalid"),
+    TOKEN_IS_EXPIRED(-105, "Token expired")
 }
