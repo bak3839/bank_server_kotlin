f20f3861a751c227217df062e85db08ea00a7473
bak3839 <bak3839@naver.com>
2025-04-23 21:52:02 +0900

    HttpClient & CustomException 구현

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,9 +38,9 @@ dependencies {
     // mongoDB
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-//
-//    // client
-//    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+
+    // client
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 //
 //    // ulid
 //    implementation("com.github.f4b6a3:ulid-creator:5.2.3")
