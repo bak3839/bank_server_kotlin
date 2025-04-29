8edeb9a8d43fab31badb5e4839fd110ca399c225
bak3839 <bak3839@naver.com>
2025-04-29 15:29:58 +0900

    kafka producer 초기 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,9 +48,9 @@ dependencies {
     // redis
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.redisson:redisson-spring-boot-starter:3.36.0")
-//
-//    // kafka
-//    implementation("org.springframework.kafka:spring-kafka:3.1.0")
+
+    // kafka
+    implementation("org.springframework.kafka:spring-kafka:3.1.0")
 
     testImplementation(kotlin("test"))
 }
